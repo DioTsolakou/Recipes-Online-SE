ae35a80dba27b1d3f5571afdaeb7abfb2408893d
Vassilis <bzafiris@gmail.com>
2020-04-10 20:24:23 +0300

    Renamed to team9 app

--- android/app/src/androidTest/java/com/example/recipesonline/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/example/recipesonline/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.recipesonline;
 
 import android.content.Context;
 
--- android/app/src/main/java/com/example/recipesonline/MainActivity.java
+++ android/app/src/main/java/com/example/recipesonline/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.recipesonline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
