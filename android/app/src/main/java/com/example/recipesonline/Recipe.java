6b6cdece62c000fbd74910e2d1ea879727cdb3a3
chrisargy <chrisargy@hotmail.gr>
2020-04-16 19:42:44 +0300

    Started the project

---
+++ android/app/src/main/java/com/example/recipesonline/Recipe.java
@@ -0,0 +1,37 @@
+package main.java.com.example.recipesonline;
+
+public class Recipe {
+
+    private String name;
+    private String description;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    
+
+
+
+}
