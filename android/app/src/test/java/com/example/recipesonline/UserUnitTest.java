b1460407c3da62c8d7f3790894617ef86a7ba4aa
chara <chara.gergi@gmail.com>
2020-04-24 20:17:20 +0300

    2

--- android/app/src/test/java/com/example/recipesonline/UserUnitTest.java
+++ android/app/src/test/java/com/example/recipesonline/UserUnitTest.java
@@ -10,46 +10,16 @@ import java.util.List;
 public class UserUnitTest
 {
     User newUser = new User();
-    RegisteredUser registeredUser = new RegisteredUser("test", "test", "test");
+    RegisteredUser rUser;
     Admin newAdmin = new Admin("test", "test", "test");
+    Recipe recipe;
 
-    @Test
-    public void registerTest()
-    {
-        newUser.register("test", "test", "test", "test");
-        Assert.assertEquals(1, MainActivity.RegisteredUsers.size());
-    }
 
-    @Test
-    public void loginTest()
-    {
-        newUser.register("test", "test", "test", "test");
-        RegisteredUser rUser = (RegisteredUser)newUser.login("test", "test");
-        Assert.assertEquals(1, MainActivity.LoggedInRegisteredUsers.size());
-    }
-
-    @Test
-    public void logoutTest()
-    {
-        newUser.register("test", "test", "test", "test");
-        RegisteredUser ru = (RegisteredUser) newUser.login("test", "test");
-        ru.logoutUser();
-        Assert.assertEquals(0, MainActivity.LoggedInRegisteredUsers.size());
-    }
-
-    @Test
-    public void registerAdminTest() {
-
-        MainActivity.Admins.add(newAdmin);
-        newAdmin.register("test1", "test1", "test1", "test1");
-        Assert.assertEquals(2, MainActivity.Admins.size());
-    }
 
     @Before
-    public void createRecipe(){
+    public void createRecipeTest(){
         newUser.register("test", "test", "test", "test");
-
-        RegisteredUser rUser = (RegisteredUser)newUser.login("test", "test");
+        rUser = (RegisteredUser)newUser.login("test", "test");
 
         Ingredient ingredient = new Ingredient("ingr", 100);
         Ingredient ingredient1 = new Ingredient("ingr1", 25);
@@ -71,20 +41,42 @@ public class UserUnitTest
         types.add(type2);
 
 
-        Recipe recipe = new Recipe(registeredUser, "name", test, "desc", types);
+        recipe = new Recipe(rUser, "name", test, "desc", types);
     }
     @Test
+    public void registerTest()
+    {
+        Assert.assertEquals(1, MainActivity.RegisteredUsers.size());
+    }
+
+    @Test
+    public void loginTest()
+    {
+        Assert.assertEquals(1, MainActivity.LoggedInRegisteredUsers.size());
+    }
+
+    @Test
+    public void logoutTest()
+    {
+        rUser.logoutUser();
+        Assert.assertEquals(0, MainActivity.LoggedInRegisteredUsers.size());
+    }
+
+    @Test
+    public void registerAdminTest() {
+        MainActivity.Admins.add(newAdmin);
+        newAdmin.register("test1", "test1", "test1", "test1");
+        Assert.assertEquals(2, MainActivity.Admins.size());
+    }
+
+    @Test
     public void searchTest() {
+
     }
 
     @Test
     public void calcCaloriesTest()
     {
-        RegisteredUser registeredUser = new RegisteredUser("test", "test", "test");
-
-        Recipe recipe = new Recipe(registeredUser, "name", test, "desc", types);
-
         Assert.assertEquals(1.25, recipe.calcCalories(), 0.0);
-
     }
 }
