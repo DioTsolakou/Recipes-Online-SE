598f240a9aa255cfc5c8a46c01e991b94ed43e85
Diogenis <dio.tsolakou@hotmail.com>
2020-05-16 19:34:04 +0300

    Partial Home UI, Login UI and Sign Up UI

---
+++ android/app/src/main/java/com/example/recipesonline/view/CreateRecipe.java
@@ -0,0 +1,14 @@
+package com.example.recipesonline.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.recipesonline.R;
+
+public class CreateRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_recipe);
+    }
+}
